7a737cf2707394c96cf59a015e507666325f4d5a
ushzkakoj <146225027+ushzkakoj@users.noreply.github.com>
2025-07-21 19:17:36 +0200

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+const textInput = document.getElementById("textInput");
+const changeButton = document.getElementById("changeButton");
+
+changeButton.addEventListener("click", () => {
+  changeButton.textContent = textInput.value;
+});
+
+const mainImage = document.getElementById("mainImage");
+mainImage.src = "./img/sonic.png";
+
+const mainLink = document.getElementById("mainLink");
+const linkImage = document.getElementById("linkImage");
+
+mainLink.href = "https://google.com";
+linkImage.alt = "Це логотип Google";
+
+const list = document.getElementById("list");
+const firstItem = list.querySelector("li");
+firstItem.textContent = "voistinumi";
